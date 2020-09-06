c4cd8cef85c211c484968702b2d48889410ba74d
Ederson Henrique Sá <edersonhs3217@gmail.com>
2020-09-06 10:30:43 -0300

    scanner update
    Agora ignora blocos de comentario.

--- scanner.py
+++ scanner.py
@@ -3,26 +3,32 @@ from linguagem import *
 
 
 def analisador(codigo):
+    ignore = False
     for linha, conteudo in enumerate(codigo):
         palavras = conteudo.split(' ')
         for palavra in palavras:
-            try:
-                palavra = int(palavra)
-                escreveLog(linha, 'INTEIRO', palavra)
-            except Exception:
+            if type(palavra) == str and palavra.strip() == comentario_fim:   # Fim comentario de mais de uma linha
+                ignore = False
+            if not ignore:
                 try:
-                    palavra = float(palavra)
-                    escreveLog(linha, 'REAL', palavra)
+                    palavra = int(palavra)
+                    escreveLog(linha, 'INTEIRO', palavra)
                 except Exception:
-                    pass
-            if type(palavra) == str and palavra.strip() in palavras_reservadas:
-                escreveLog(linha, 'PALAVRA_RESERVADA', palavra.strip())
-            if type(palavra) == str and palavra.strip() in operadores:
-                escreveLog(linha, 'OPERADOR', palavra.strip())
-            if type(palavra) == str and palavra.strip() in delimitadores:
-                escreveLog(linha, 'DELIMITADOR', palavra.strip())
-            if type(palavra) == str and palavra == '//':   # comentario de uma linha
-                break
-            if type(palavra) == str and len(palavra) != 0 and palavra.strip() not in palavras_reservadas and \
-                    palavra.strip() not in delimitadores and palavra.strip() not in operadores:
-                escreveLog(linha, 'IDENTIFICADOR', palavra)
+                    try:
+                        palavra = float(palavra)
+                        escreveLog(linha, 'REAL', palavra)
+                    except Exception:
+                        pass
+                if type(palavra) == str and palavra.strip() in palavras_reservadas:
+                    escreveLog(linha, 'PALAVRA_RESERVADA', palavra.strip())
+                if type(palavra) == str and palavra.strip() in operadores:
+                    escreveLog(linha, 'OPERADOR', palavra.strip())
+                if type(palavra) == str and palavra.strip() in delimitadores:
+                    escreveLog(linha, 'DELIMITADOR', palavra.strip())
+                if type(palavra) == str and palavra.strip() == comentario:   # comentario de uma linha
+                    break
+                if type(palavra) == str and palavra.strip() == comentario_inicio:   # Comentario de mais de uma linha
+                    ignore = True
+                if type(palavra) == str and len(palavra) != 0 and palavra.strip() not in palavras_reservadas and \
+                        palavra.strip() not in delimitadores and palavra.strip() not in operadores:
+                    escreveLog(linha, 'IDENTIFICADOR', palavra)
