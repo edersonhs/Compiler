9f8d5017aaa221c8fd9b2d3ecdb82cdb07871644
Ederson Henrique Sá <edersonhs3217@gmail.com>
2020-09-27 15:42:17 -0300

    scanner update

--- linguagem.py
+++ linguagem.py
@@ -5,7 +5,7 @@ comentario = '//'
 comentario_inicio = '/*'
 comentario_fim = '*/'
 
-delimitadores = (';', '{', '}', '(', ')', '[', ']', comentario, comentario_inicio, comentario_fim, '“', "‘")
+delimitadores = (';', '{', '}', '(', ')', '[', ']', comentario, comentario_inicio, comentario_fim, '“', "‘", ",")
 
 palavras_reservadas = ('int', 'float', 'string', 'boolean', 'char', 'void', 'double', 'public', 'private', 'igor',
                        'vasco', 'return', 'if', 'else', 'for', 'while', 'break', 'continue', 'funcao', 'hame', 'true',
--- scanner.py
+++ scanner.py
@@ -1,24 +1,57 @@
 from log import *
 from linguagem import *
 
+ignore = False
+comment = False
+
 
 def deep_analyzer(linha, palavra):   # Caracter por caracter
-    # for indice, caractere in enumerate(palavra):
+    global ignore
+    global comment
+    """
     if str(palavra[0]).isnumeric():
         escreveLog(linha, f'{cores["vermelho"]}INVALIDO', palavra,
                    'O primeiro caractere de um identificado não pode ser um número.')
     else:
         if str(palavra).isalpha():
             escreveLog(linha, 'IDENTIFICADOR', palavra)
+    """
+    aux = ''
+    caractere = 0
+    if comentario_fim in palavra:
+        escreveLog(linha, 'DELIMITADOR', comentario_fim)
+        caractere = palavra.find(comentario_fim) + 2
+
+    for caractere in range(caractere, len(palavra)):
+        if palavra[caractere].isalpha() or palavra[caractere].isdigit():
+            aux += palavra[caractere]
+        else:
+            break
+
+    if len(aux.split()) != 0:
+        escreveLog(linha, 'IDENTIFICADOR', aux)
+
+    if caractere >= len(palavra):   # Se já tiver percorrido todo o indice da string, sai da função.
+        return
+    if caractere+2 <= len(palavra):
+        if palavra[caractere:caractere+2] == comentario:
+            comment = True
+            escreveLog(linha, 'DELIMITADOR', comentario)
+            return
+        if palavra[caractere:caractere+2] == comentario_inicio:
+            ignore = True
+            escreveLog(linha, 'DELIMITADOR', comentario_inicio)
+            return
 
 
 def analisador(codigo):
-    ignore = False
+    global ignore
+    global comment
     for linha, conteudo in enumerate(codigo):
         palavras = conteudo.split(' ')
         palavras = [palavras.strip() for palavras in palavras]   # retirando os espaços do inicio e fim de cada palavra
         for palavra in palavras:
-            if type(palavra) == str and palavra.strip() == comentario_fim:   # Fim comentario de mais de uma linha
+            if type(palavra) == str and comentario_fim in palavra.strip():   # Fim comentario de mais de uma linha
                 ignore = False
             if not ignore:
                 try:
@@ -45,3 +78,6 @@ def analisador(codigo):
                             palavra not in delimitadores and palavra not in operadores:   # Identificador
                         deep_analyzer(linha, palavra)
                         # escreveLog(linha, 'IDENTIFICADOR', palavra)
+                        if comment:
+                            comment = False   # Controle de comentario de uma linha dentro do deep_analyzer
+                            break
